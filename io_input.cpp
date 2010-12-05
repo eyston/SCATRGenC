acaa7214f97be7919be47072f2812a5cb15aacd2
Erik Petersen <Huey@Erik-Petersens-MacBook-Pro.local>
2010-12-05 17:22:57 -0500

    missed a few files, I suck

--- io_input.cpp
+++ io_input.cpp
@@ -1,24 +1,9 @@
-struct SimulationSetup
+SimulationParameters io_input_params(const char *input_file_name)
 {
-	float t0, tstop, dt, tinc;
-	float dtout, dtdump;
-	bool lflag[6];
-	char lflag_c[6];
-	float rmin, rmax, rmaxu, qmin;
-	bool lclose;
-	char lclose_c;
-	float rcrit;
-	char output_file_name[256];
-	char frame[4];
-	char fopenstat[8];	
-};
-
-SimulationSetup io_input(const char *input_file_name)
-{
-	SimulationSetup setup = SimulationSetup();
+	SimulationParameters setup = SimulationParameters();
 
 	FILE *input_file;
-	input_file = fopen("param.in", "r");
+	input_file = fopen(input_file_name, "r");
 
 	fscanf(input_file, "%f %f %f %f\n", &setup.t0, &setup.tstop, &setup.dt, &setup.tinc);
 	fscanf(input_file, "%f %f\n", &setup.dtout, &setup.dtdump);
@@ -51,4 +36,42 @@ SimulationSetup io_input(const char *input_file_name)
 	printf("%s\n", setup.fopenstat);
 	
 	return setup;
+}
+
+NBodyParameters io_input_planets(const char *input_file_name)
+{
+	FILE *input_file;
+	input_file = fopen(input_file_name, "r");
+
+	NBodyParameters pl_params = NBodyParameters();
+	
+
+	fscanf(input_file, "%i %i\n", &pl_params.nbod, &pl_params.npl);
+	printf("%i, %i\n", pl_params.nbod, pl_params.npl);
+	
+	NBody *bodies = new NBody[pl_params.nbod];
+	pl_params.bodies = bodies;
+	
+	fscanf(input_file, "%f %f %f\n", &bodies[0].mass, &pl_params.j2rp2, &pl_params.j4rp4);
+	fscanf(input_file, "%f %f %f\n", &bodies[0].h_loc.x, &bodies[0].h_loc.y, &bodies[0].h_loc.z);
+	fscanf(input_file, "%f %f %f\n", &bodies[0].h_vel.x, &bodies[0].h_vel.y, &bodies[0].h_vel.z);
+
+	printf("%f, %f, %f\n", bodies[0].mass, pl_params.j2rp2, pl_params.j4rp4);
+	printf("%f, %f, %f\n", bodies[0].h_loc.x, bodies[0].h_loc.y, bodies[0].h_loc.z);
+	printf("%f, %f, %f\n", bodies[0].h_vel.x, bodies[0].h_vel.y, bodies[0].h_vel.z);
+	
+	for(int i=1; i < pl_params.nbod; i++)
+	{
+		fscanf(input_file, "%f %f\n", &bodies[i].mass, &bodies[i].rpl);
+		fscanf(input_file, "%f %f %f\n", &bodies[i].h_loc.x, &bodies[i].h_loc.y, &bodies[i].h_loc.z);
+		fscanf(input_file, "%f %f %f\n", &bodies[i].h_vel.x, &bodies[i].h_vel.y, &bodies[i].h_vel.z);
+
+		printf("%f, %f\n", bodies[i].mass, bodies[i].rpl);
+		printf("%f, %f, %f\n", bodies[i].h_loc.x, bodies[i].h_loc.y, bodies[i].h_loc.z);
+		printf("%f, %f, %f\n", bodies[i].h_vel.x, bodies[i].h_vel.y, bodies[i].h_vel.z);		
+	}
+	
+	fclose(input_file);
+	
+	return pl_params;
 }
--- main.cpp
+++ main.cpp
@@ -2,90 +2,26 @@
 #include <fstream>
 #include <stdio.h>
 
+#include "structures.h"
+#include "coord_h2j.h"
 #include "io_input.cpp"
+#include "corrector.cpp"
 
 using namespace std;
 
-#define iterations 1
-#define N 5
-
-void coord_h2j(int nbod, float mass[], float xh[], float yh[], float zh[],
-									   float vxh[], float vyh[], float vzh[],
-									   float xj[], float yj[], float zj[],
-									   float vxj[], float vyj[], float vzj[])
-{
-	float *eta = new float[nbod];
-	float sumx, sumy, sumz, sumvx, sumvy, sumvz;
-	float capx, capy, capz, capvx, capvy, capvz;
-	
-	eta[0] = mass[0];
-	
-	for(int i=1; i<nbod; i++)
-	{
-		eta[i] = eta[i-1] + mass[i];
-	}
-	
-	xj[0] = 0.0;
-	yj[0] = 0.0;
-	zj[0] = 0.0;
-	vxj[0] = 0.0;
-	vyj[0] = 0.0;
-	vzj[0] = 0.0;
-	
-	xj[1] = xh[1];
-	yj[1] = yh[1];
-	zj[1] = zh[1];
-	vxj[1] = vxh[1];
-	vyj[1] = vyh[1];
-	vzj[1] = vzh[1];
-	
-	sumx = mass[1]*xh[1];
-	sumy = mass[1]*yh[1];
-	sumz = mass[1]*zh[1];
-	sumvx = mass[1]*vxh[1];
-	sumvy = mass[1]*vyh[1];
-	sumvz = mass[1]*vzh[1];
-	
-	capx = sumx/eta[1];
-	capy = sumy/eta[1];
-	capz = sumz/eta[1];
-	capvx = sumvx/eta[1];
-	capvy = sumvy/eta[1];
-	capvz = sumvz/eta[1];
-	
-	for(int i=2; i<nbod; i++)
-	{
-		xj[i] = xh[i] - capx;
-		yj[i] = yh[i] - capy;
-		zj[i] = zh[i] - capz;
-		vxj[i] = vxh[i] - capvx;
-		vyj[i] = vyh[i] - capvy;
-		vzj[i] = vzh[i] - capvz;
-		
-		if(i < (nbod-1))
-		{
-			sumx = sumx + mass[i]*xh[i];
-			sumy = sumy + mass[i]*yh[i];
-			sumz = sumz + mass[i]*zh[i];
-			sumvx = sumvx + mass[i]*vxh[i];
-			sumvy = sumvy + mass[i]*vyh[i];
-			sumvz = sumvz + mass[i]*vzh[i];
-			
-			capx = sumx/eta[i];
-			capy = sumy/eta[i];
-			capz = sumz/eta[i];
-			capvx = sumvx/eta[i];
-			capvy = sumvy/eta[i];
-			capvz = sumvz/eta[i];
-		}
-	}
-
-	delete[] eta;
-}
 
 int main()
 {
 
-	SimulationSetup setup = io_input("param.in");
+	SimulationParameters params = io_input_params("param.in");
+	
+	NBodyParameters pl_params = io_input_planets("pl.in");
+	
+	int t = params.t0;
+	int tout = params.t0 + params.dtout;
+	int tdump = params.t0 + params.dtdump;
+	
+	invcorrector(pl_params.nbod, pl_params.bodies, params.tinc, params.dt);
+	
 	return 0;
 }
