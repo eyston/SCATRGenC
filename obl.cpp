7c858ac5632c6b95bd47656220ca9e7ab28c8906
unknown <Huey@.(none)>
2010-12-08 00:21:16 -0500

    cleaning up

--- obl.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "structures.h"
-#include "obl.h"
-
-void obl_acc(int nbod, float *mass, float j2rp2, float j4rp4, float *xh, float *yh, float *zh, float *irh, float aoblx, float aobly, float aoblz)
-{
-	printf("obl_acc called \n");
-}
